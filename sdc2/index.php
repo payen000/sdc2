0ee0ec7a292dffe6ef155c15b24ec37dafa71893
payen0 <49595803+payen0@users.noreply.github.com>
2021-08-04 00:13:52 -0600

    Add files via upload

--- sdc2/index.php
+++ sdc2/index.php
@@ -1,5 +1,240 @@
-<?php
-    $url = "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-    $escaped_url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
-    header('Location: http://' . $url . 'home/landing/');
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . '/sdc2/include/head.php');
+    ?>
+    <!-- NAVBAR
+    ================================================== -->
+    <body>        
+        <?php
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/sdc2/include/navbar.php');
+        ?>
+
+        <!-- inicio content -->
+
+        <div class="content">
+
+            <!-- header principal -->
+
+            <div class='main-header'>
+                <div class='registro'>
+                    <div class='row centered'>
+                        <h2>2º. Simposio de Divulgación de la ciencia</h2>
+                    </div>
+                    <div class='row centered'>
+                        <h3>una emergencia climática</h3>
+                    </div>
+
+                    <div class='row centered'>
+                        <h4>3, 4 y 5 de noviembre de 2021</h4>
+                    </div>
+                            
+                    <div class='row centered'><span>
+                        <a class="btn btn-primary" href="#" role="button">Registro</a></span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- fin header -->
+
+            <hr class='separador' id='presentacion'>
+
+            <!-- inicio introduccion -->
+
+            <div class="row centered">
+                <h2><span>Presentación</span></h2>
+            </div>
+
+            <div class="row">
+                <p>
+                    De acuerdo con datos del Instituto Nacional de Estadística y Geografía (INEGI) 2014, el 6.4% de la población del país reportaron tener al menos una discapacidad. El 10% de los mexicanos son indígenas que tienen sus propias lenguas. Por su parte los resultados del Censo Nacional de Gobierno, Seguridad Pública y Sistema Penitenciario Estatales (CNGSPSPE) 2016, mostraron que había un total de 217,595 reclusos en los Centros Penitenciarios en nuestro país. En cuestión de personas en situación de calle el Diagnóstico Situacional de las Poblaciones Callejeras 2017-2018 reporta 6,754 personas tan solo en la Ciudad de México. El CONEVAL corelacióna hogares con menor ingreso y la presencia de personas con discapacidad. Correlaciones similares son encontradas entre pobreza y delincuencia, entre población indígena y pobresa, entre población indígena y discapacidad y generalmente la vulnerabilidad de esta población se amplifica con la falta de cultura científica, entre otros factores. <br><br>
+
+                    En resumen, una parte importante de la población Mexicana no cuenta con los mismos recursos físicos, congnitivos, económicos, de lenguaje y de libertad, para los cuales se requiere el diseñados de programas de divulgación de la ciencia que consideren las particularidades y limitantes de comunicación y acceso en cada caso. Si queremos una sociedad mas justa y con mayor igualdad, independientemente de las capacidades, la geografía, el idioma, la condición de salud o la economía, es importante compartir el conocimiento con todos y cada uno de los mexicanos. Con este fin se plantea la realización del “2o. Simposio: Divulgación de la Ciencia y la Tecnología: una emergencia” en el cual se espera que divulgadores de todo el país contribuyan a reflexionar y proponer los mecanismos necesarios para asegurar que el conocimiento llegue a ciertos grupos de la población, y así cambiar su situación de vulnerabilidad.
+                </p>
+            </div>
+            <!-- fin introduccion -->
+
+            <hr class='separador' id='objetivos'>
+
+            <!-- inicio objetivos -->
+            <div  class="row centered">
+                <h2><span>Objetivos generales</span></h2>
+            </div>
+
+            <div class="row centered">
+                <div class="col-12">
+                    <p>El simposio tiene como objetivos: </p>
+                    <ul class="lead left">
+                        <li>  Crear un espacio de intercambio de experiencias, metodologías, técnicas, ideas, reflexiones y resultados prácticos, respecto a la divulgación de la ciencia y la tecnología.</li>
+                        <li> Reconocer la importancia de la divulgación de la ciencia en la creación de conciencia sobre la emergencia climática.  </li>
+                        <li>  Establecer un dialogo entre especialistas en el cambio climático y divulgadores de la ciencia con el fin de diseñar estrategias para la comunicación de la emergencia. </li>
+                        <li> Cuerpos menores del Sistema Solar </li>
+                        <li> Aprovechar las herramientas de tecnologías de la información y la comunicación para crear nexos nacionales e internacionales entre grupos interdisciplinarios civiles, gubernamentales y académicos.  </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <!-- fin objetivos -->
+
+            <hr class='separador' id='temas'>
+
+            <!-- inicio temas -->
+            <div class="row centered">
+                <h2><span>Temas</span></h2>
+            </div>
+
+            <div class="row centered">
+                <p>Los trabajos a ser presentados podrán referirse la experiencias, metodología, perspectivas, investigación, evaluación y resultados de divulgación de la ciencia en las siguientes líneas temáticas:</p>
+                
+                <div class="col-12">
+                    <ul class="lead left">
+                        <li>  ¿Qué es el cambio climático y cómo comunicarlo? - esto incluye, causas, escenarios</li>
+                        <li>  Cambio climático y desastres, cuáles son los impactos, cómo comunicar asertivamente </li>
+                        <li> Mitigación: el papel de la energía y la industria </li>
+                        <li>  Mitigación: aspectos de desarrollo forestal y agropecuario </li>
+                        <li>  Adaptación y construcción de resiliencia socio-ambiental </li>
+                        <li> Adaptación y reducción de vulnerabilidad social (aquí estarían temas de pobreza, desigualdad) </li>
+                        <li>  Las ciudades frente al cambio climático (aquí el foco es lo urbano, y puede hablarse de mitigación y adaptación)  </li>
+                        <li>  La biodiversidad y las soluciones basadas en la naturaleza para enfrentar el cambio climático </li>
+                        <li> Economía y cambio climático.  </li>
+                    </ul>
+                </div>                
+            </div>
+            
+            <!-- fin temas -->
+
+            <hr class='separador' id='fechas'>
+
+            <!-- inicio fechas -->
+            <div  class="row centered">
+                <h2><span>Fechas Importantes</span></h2>
+            </div>
+
+            <div class="row centered">
+                <div class="col-12">
+                    <ul>
+                        <li>Fecha límite para registrar trabajos al Simposio:<span class ="fecha"> 19 de septiembre de 2021</span></li>
+                        <li>Publicación de trabajos aceptados:<span class = "fecha"> 3 de octubre de 2021</span></li>
+                        <li>Fecha límite para retirar trabajos del Simposio: <span class = "fecha">15 de octubre de 2021</span></li>
+                        <li>Publicación del programa final: <span class = "fecha">22 de octubre de 2021</span></li>
+                        <li>Simposio: <span class = "fecha">3, 4 y 5 de noviembre de 2021</span></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- fin fechas -->
+
+            <hr class='separador' id='programa'>
+
+            <!-- inicio programa -->
+            <div class="row centered">
+                <h2><span>Programa</span></h2>
+            </div>
+
+            <table class="table table-light">
+              <thead>
+                <tr>
+                  <th scope="row"></th>
+                  <td colspan="1">3 Nov</td>
+                  <td>4 Nov</td>
+                  <td>5 Nov</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">9:00</th>
+                  <td class='seleccionable'>plática 1</td>
+                  <td class='seleccionable'>plática 4</td>
+                  <td class='seleccionable'>plática 7</td>                  
+                </tr>
+                <tr>
+                  <th scope="row">10:30</th>
+                  <td class='seleccionable' colspan="1">plática 2</td>
+                  <td class='seleccionable'>plática 5</td>
+                  <td class='seleccionable'>plática 8</td>
+                </tr>
+                <tr>
+                  <th scope="row">12:00</th>
+                  <td class='seleccionable' colspan="1">plática 3</td>
+                  <td class='seleccionable'>plática 6</td>
+                  <td class='seleccionable'>plática 9</td>
+                </tr>
+              </tbody>
+            </table>
+
+
+            <!-- fin programa -->
+
+            <hr class='separador' id='comite'>
+
+            <!-- inicio comite -->
+           
+           <div class="row centered">
+                <h2><span>Comité Académico</span></h2><br>
+            </div>
+        
+            <div class='row centered'>  
+                <div class="col-12">
+                    <p class="lead">
+                        Dra. Lorena Arias Montaño, Coordinadora Centro Astronómico Clavius <br>
+                        Dra. Julia Carabias Lillo, El Colegio Nacional-Facultad de Ciencias, UNAM <br>
+                        Dr. Ernesto Márquez Nerey- Presidente de la Sociedad Mexicana para la Divulgación de la Ciencia y la Técnica (SOMEDICYT) <br>
+                        Dr. Juan Manuel Núñez Hernández, CENTRUS
+                    </p>
+                </div>
+            </div><br>
+
+            <div class="row centered">
+                <h2><span>Comité Local</span></h2><br>
+            </div>
+        
+        <div class='row centered'>
+            <div class="col-12">
+                <p class="lead">
+                    Dra. Anabel Arrieta Ostos<br>
+                    Dra. Dominique Brun Battistini (Directora del Departamento de Física y Matemáticas)<br>
+                    Dr. José Alberto Lara Pulido <br>
+                    Dr. Gerardo Martínez <br>
+                    Ing. Agustín Payen Sandoval <br>
+                    Mtra. María Zorrilla Ramos
+                </p>
+            </div>
+        </div>
+        <!-- fin comite -->
+
+        <hr class='separador' id='contacto'>
+
+        <!-- inicio contacto -->
+        <div class="row centered">
+            <h2>Contacto</h2>
+        </div>
+
+        <div class='row centered'>
+            <div class="col-12">
+                <p class="lead">
+                    Anabel Arrieta<br>
+                    anabel.arrieta@ibero.mx<br><br>
+
+                    Facebook<br>
+                    Divulgacion2021.ibero<br><br>
+                </p>
+            </div>
+        </div>
+            
+        <!-- fin contacto -->
+
+
+        <hr class='separador'>
+
+        <!-- FOOTER -->
+        
+        <?php
+            require_once ($_SERVER['DOCUMENT_ROOT'] . '/sdc2/include/footer.php');
+        ?>
+
+        </div>
+        <!-- fin content -->
+
+    
+    </body>
+</html>
